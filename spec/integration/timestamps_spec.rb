69c18b9284be5dfa6c1938b4fe23e34cd0b50340
Dan Kubb <dan.kubb@gmail.com>
2011-02-24 19:54:54 -0800

    Rubinius does not work when stubbing out DateTime.now or Date.today

--- spec/integration/timestamps_spec.rb
+++ spec/integration/timestamps_spec.rb
@@ -46,18 +46,22 @@ describe 'DataMapper::Timestamp' do
         green_smoothie.save
         green_smoothie.updated_at.should be_a_kind_of(DateTime)
         green_smoothie.updated_on.should be_a_kind_of(Date)
-        original_updated_at = green_smoothie.updated_at
-        original_updated_on = green_smoothie.updated_on
-        time_tomorrow = DateTime.now + 1
-        date_tomorrow = Date.today + 1
-        DateTime.stub!(:now).and_return { time_tomorrow }
-        Date.stub!(:today).and_return { date_tomorrow }
-        green_smoothie.name = 'Cranberry Mango'
-        green_smoothie.save
-        green_smoothie.updated_at.should_not eql(original_updated_at)
-        green_smoothie.updated_on.should_not eql(original_updated_on)
-        green_smoothie.updated_at.should eql(time_tomorrow)
-        green_smoothie.updated_on.should eql(date_tomorrow)
+
+        # rubinius does not work with rspec class method stubs
+        unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+          original_updated_at = green_smoothie.updated_at
+          original_updated_on = green_smoothie.updated_on
+          time_tomorrow = DateTime.now + 1
+          date_tomorrow = Date.today + 1
+          DateTime.stub!(:now).and_return { time_tomorrow }
+          Date.stub!(:today).and_return { date_tomorrow }
+          green_smoothie.name = 'Cranberry Mango'
+          green_smoothie.save
+          green_smoothie.updated_at.should_not eql(original_updated_at)
+          green_smoothie.updated_on.should_not eql(original_updated_on)
+          green_smoothie.updated_at.should eql(time_tomorrow)
+          green_smoothie.updated_on.should eql(date_tomorrow)
+        end
       end
 
       it "should only set the updated_at/on fields on dirty objects" do
@@ -67,32 +71,39 @@ describe 'DataMapper::Timestamp' do
         green_smoothie.save
         green_smoothie.updated_at.should be_a_kind_of(DateTime)
         green_smoothie.updated_on.should be_a_kind_of(Date)
-        original_updated_at = green_smoothie.updated_at
-        original_updated_on = green_smoothie.updated_on
-        time_tomorrow = DateTime.now + 1
-        date_tomorrow = Date.today + 1
-        DateTime.stub!(:now).and_return { time_tomorrow }
-        Date.stub!(:today).and_return { date_tomorrow }
-        green_smoothie.save
-        green_smoothie.updated_at.should_not eql(time_tomorrow)
-        green_smoothie.updated_on.should_not eql(date_tomorrow)
-        green_smoothie.updated_at.should eql(original_updated_at)
-        green_smoothie.updated_on.should eql(original_updated_on)
-      end
 
-      describe '#touch' do
-        it 'should update the updated_at/on fields' do
-          green_smoothie = GreenSmoothie.create(:name => 'Mango')
-
+        # rubinius does not work with rspec class method stubs
+        unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+          original_updated_at = green_smoothie.updated_at
+          original_updated_on = green_smoothie.updated_on
           time_tomorrow = DateTime.now + 1
           date_tomorrow = Date.today + 1
           DateTime.stub!(:now).and_return { time_tomorrow }
           Date.stub!(:today).and_return { date_tomorrow }
+          green_smoothie.save
+          green_smoothie.updated_at.should_not eql(time_tomorrow)
+          green_smoothie.updated_on.should_not eql(date_tomorrow)
+          green_smoothie.updated_at.should eql(original_updated_at)
+          green_smoothie.updated_on.should eql(original_updated_on)
+        end
+      end
 
-          green_smoothie.touch
+      describe '#touch' do
+        # rubinius does not work with rspec class method stubs
+        unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+          it 'should update the updated_at/on fields' do
+            green_smoothie = GreenSmoothie.create(:name => 'Mango')
 
-          green_smoothie.updated_at.should eql(time_tomorrow)
-          green_smoothie.updated_on.should eql(date_tomorrow)
+            time_tomorrow = DateTime.now + 1
+            date_tomorrow = Date.today + 1
+            DateTime.stub!(:now).and_return { time_tomorrow }
+            Date.stub!(:today).and_return { date_tomorrow }
+
+            green_smoothie.touch
+
+            green_smoothie.updated_at.should eql(time_tomorrow)
+            green_smoothie.updated_on.should eql(date_tomorrow)
+          end
         end
 
         it 'should not update the created_at/on fields' do
